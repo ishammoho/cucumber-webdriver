6ead7dbdb8ca7e4b7f9b5d5336f36381e80c12fb
ishammoho <ishammoho@gmail.com>
2015-04-17 15:15:23 +1200

    Created new feature file and supporting code to verify Assuirty Home page

---
+++ src/test/java/Cucumber/BasicCalculator/AssurityFunctionStepDefs.java
@@ -0,0 +1,34 @@
+package Cucumber.BasicCalculator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+//import pageObjects.GoogleHomePage;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import java.util.NoSuchElementException;
+
+public class AssurityFunctionStepDefs {
+
+	public WebDriver driver;
+	
+	@Given("^I go to Assuirty Home page$")
+	public void goToAssuirtyHomepage() {
+		this.driver=new FirefoxDriver();
+		driver.get("http://www.assurity.co.nz");
+	}
+
+	@Then("^I can see Assurity Logo in the Home page$")
+	public void verifyLogoIsDisplayed() {
+		try {
+			driver.findElement(By.xpath("//a/img[@alt='Assurity - Confidence in software']")).isDisplayed();
+		}catch (NoSuchElementException e){
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
+
+}
---
+++ src/test/resources/verify_assurity_home_page.feature
@@ -0,0 +1,9 @@
+@VerifyAssurityHomePage
+Feature: Testing Assurity Home Page
+  As a user
+  I want to see Assurity Logo
+  So that I can verify that Assurity Home page displays correctly
+
+  Scenario: Verify Assurity Logo is displayed in Assurity Home page
+    Given I go to Assuirty Home page
+    Then I can see Assurity Logo in the Home page
